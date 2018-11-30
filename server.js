588b07aecd5357cced061e1fb299db57f7bbcedc
magicbowen <e.bowen.wang@icloud.com>
2018-11-30 15:31:38 +0800

    modify code

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const Query = require('darwin-sdk').Query
 const OpenSkillEvent = require('darwin-sdk').OpenSkillEvent
 const QuitSkillEvent = require('darwin-sdk').QuitSkillEvent
 const NoResponseEvent = require('darwin-sdk').NoResponseEvent
+const RecordFinishEvent = require('darwin-sdk').RecordFinishEvent
+const RecordFailEvent = require('darwin-sdk').RecordFailEvent
 const AixBot = require('aixbot')
 const config = require('./config')
 
@@ -20,15 +22,18 @@ function getAgentName(appId) {
     return config.agents[`${appId}`]
 }
 
-function buildAixbotReply(ctx, rsp) {
-    const instructs = rsp.getInstructs()
-    if (!instructs) return ctx.query(rsp.getReply())
+function buildAixbotReply(ctx, chatbotReply) {
+    const instructs = chatbotReply.getInstructs()
+    if (!instructs) return ctx.query(chatbotReply.getReply())
 
     let quitSkill = false
-    let response = ctx.directiveTts(rsp.getReply())
+    let response = ctx.directiveTts(chatbotReply.getReply())
     for (let instruct of instructs) {
         if(instruct.type === "play-audio") {
             response.directiveAudio(instruct['url'])
+        }
+        if(instruct.type === "play-record") {
+            response.directiveRecord(instruct['mediaId'])
         }
         if(instruct.type === "text") {
             response.directiveTts(instruct['reply'])
@@ -47,11 +52,16 @@ aixbot.use(async (ctx, next) => {
     const agent = getAgentName(ctx.request.appId)
     const chatbot = agent ? new Chatbot(config.chatbot_url, agent, config.source) : null
     ctx.handleEvent = async (event) => {
-        if(!chatbot){
+        if(!chatbot) {
+            console.error('ERROR: found no agent')
             return ctx.reply("抱歉，没有找到技能").closeSession();
         }
-        const rsp = await chatbot.dispose(event)
-        return buildAixbotReply(ctx, rsp)
+
+        console.log(`receive: ${JSON.stringify(event.body)}`)
+        const chatbotReply = await chatbot.dispose(event)
+        const result = buildAixbotReply(ctx, chatbotReply)
+        console.log(`reply : ${JSON.stringify(result)}`)
+        return result
     }
     await next()
 })
@@ -71,6 +81,17 @@ aixbot.onEvent('quitSkill', async (ctx) => {
 aixbot.onEvent('inSkill', async (ctx) => {
     await ctx.handleEvent(new Query(ctx.request.user.user_id, ctx.request.query))
 })
+
+aixbot.onEvent('recordFinish', async (ctx) => {
+    const userId = ctx.request.user.user_id
+    const asrText = ctx.request.eventProperty.asr_text
+    const mediaId = ctx.request.eventProperty.msg_file_id
+    await ctx.handleEvent(new RecordFinishEvent(userId, mediaId, asrText));
+});
+
+aixbot.onEvent('recordFail', async (ctx) => {
+    await ctx.handleEvent(new RecordFailEvent(ctx.request.user.user_id));
+});
 
 aixbot.onError((err, ctx) => {
     ctx.reply('内部错误，稍后再试').closeSession()
